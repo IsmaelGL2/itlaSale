fab88b4c18551bb1470e42d38e6df07f4c61600e
K <K@DESKTOP-C6MC1E9>
2023-09-28 18:43:49 -0400

    1

--- Sale/Sale.Infrastructure/Interfaces/IProductoRepository.cs
+++ Sale/Sale.Infrastructure/Interfaces/IProductoRepository.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using Sale.Domain.Entities;
+using Sale.Domain.Repository;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Sale.Infrastructure.Interfaces
 {
-    internal interface IProductoRepository
+    public interface IProductoRepository : IBaseRepository<Producto>
     {
+
     }
 }
--- Sale/Sale.Infrastructure/Repositories/ProductoRepository.cs
+++ Sale/Sale.Infrastructure/Repositories/ProductoRepository.cs
@@ -1,10 +1,36 @@
-﻿using System;
+﻿using Sale.Domain.Entities;
+using Sale.Infrastructure.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Sale.Infrastructure.Repositories
 {
-    internal class ProductoRepository
+    internal class ProductoRepository : IProductoRepository
     {
+        public void Delete(Producto entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Producto> GetEntities()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Producto GetEntity(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save(Producto entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Producto entity)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
