e905645a6ebbf4f0d925b9830d02867cfd4bb7f2
K <K@DESKTOP-C6MC1E9>
2023-09-28 17:03:35 -0400

    1

---
+++ Sale/Sale.Domain/Core/BaseEntity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sale.Domain.Core
+{
+    public abstract class BaseEntity
+    {
+        public int? Id { get; set; }
+
+        public DateTime? FechaRegistro { get; set; }
+
+        public int? IdUsuarioCreation { get; set; }
+
+        public DateTime? FechaMod { get; set; }
+
+        public int? IdUsuarioMod { get; set; }
+
+        public int? IdUsuarioElimino { get; set; }
+
+        public DateTime? FechaElimino { get; set; }
+
+        public bool? Eliminado { get; set; }
+    }
+}
---
+++ Sale/Sale.Domain/Repository/IBaseRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sale.Domain.Repository
+{
+    internal interface IBaseRepository<TEntity> where TEntity : class
+    {
+        void Save(TEntity entity);
+        void Update(TEntity entity);
+        void Delete(TEntity entity);
+        List<TEntity> GetEntities();
+        TEntity GetEntity(int id);
+    }
+}
