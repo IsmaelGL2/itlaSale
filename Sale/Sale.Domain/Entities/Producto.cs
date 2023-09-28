5068c7f0086d65c80153a94df7ee68a05a381ab5
K <K@DESKTOP-C6MC1E9>
2023-09-28 17:51:48 -0400

    1

---
+++ Sale/Sale.Domain/Entities/Producto.cs
@@ -0,0 +1,19 @@
+﻿using Sale.Domain.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sale.Domain.Entities
+{
+    public class Producto : BaseEntity
+    {
+        public string? CodigoBarra { get; set; }
+        public string? Marca { get; set; }
+        public string? Descripcion { get; set;}
+        public int? IdCategoria { get; set;}
+        public int? Stock { get; set; }
+        public string? UrlImagen { get; set;}
+        public string? NombreImagen { get; set;}
+        public double? Precio { get; set;}
+    }
+}
---
+++ Sale/Sale.Domain/Repository/IRepositoryProducto.cs
@@ -0,0 +1,16 @@
+﻿using Sale.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sale.Domain.Repository
+{
+    internal interface IRepositoryProducto
+    {
+        void Save(Producto products);
+        void Update(Producto products);
+        void Delete(Producto products);
+        List<Producto> GetProducts();
+        Producto GetById(int id);
+    }
+}
