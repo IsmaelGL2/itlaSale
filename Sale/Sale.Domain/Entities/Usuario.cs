a87ba309d0660b400bc5a42011cb8c598d551774
Cristian7hg <cristian007hg@gmail.com>
2023-10-02 13:46:01 -0400

    Entidad y Repositorio

---
+++ Sale/Sale.Domain/Entities/Usuario.cs
@@ -0,0 +1,26 @@
+﻿using Sale.Domain.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sale.Domain.Entities
+{
+    public class Usuario : BaseEntity
+    {
+        public string? Nombre { get; set; }  
+
+        public string? Correo { get; set; }
+
+        public string? Telefono { get; set; }
+
+        public int? IdRol { get; set; }
+
+        public string? UrlFoto { get; set; }
+
+        public string? NombreFoto { get; set; }
+
+        public string? Clave { get; set; }
+
+
+    }
+}
---
+++ Sale/Sale.Domain/Repository/IUsuarioRepository.cs
@@ -0,0 +1,17 @@
+﻿using Sale.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sale.Domain.Repository
+{
+    public interface IUsuarioRepository
+    {
+        void Save(Usuario usuario);
+        void Update(Usuario usuario);
+        void Remove(Usuario usuario);
+        List <Usuario> GetUsuarios();
+        Usuario GetUsuarios(int id);
+
+    }
+}
